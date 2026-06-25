5f00583761fb216cea2ffeb7831b7debee2447eb
sthalin <sthalin.11@gmail.com>
2026-06-24 21:06:07 -0500

    test 4

--- src/common/guards/supabase-auth.guard.ts
+++ src/common/guards/supabase-auth.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { createClient } from '@supabase/supabase-js';
 import { PrismaService } from '../../config/prisma/prisma.service';
+import WebSocket from 'ws'; // 👈 importa ws
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
   private supabase = createClient(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      // 👇 opción de transporte usando ws
+      realtime: {
+        transport: WebSocket as any, // 👈 casteo para evitar el error de tipos
+      },
+    }
   );
 
   constructor(private prisma: PrismaService) { }
@@ -19,13 +26,11 @@ export class SupabaseAuthGuard implements CanActivate {
     }
     const token = authHeader.split(' ')[1];
 
-    // 1. Validar token con Supabase
     const { data: { user }, error } = await this.supabase.auth.getUser(token);
     if (error || !user) {
       throw new UnauthorizedException('Token inválido');
     }
 
-    // 2. Buscar o crear usuario en tabla local
     let dbUser = await this.prisma.usuarios.findFirst({
       where: {
         OR: [
@@ -36,10 +41,8 @@ export class SupabaseAuthGuard implements CanActivate {
     });
 
     if (!dbUser) {
-      // Empresa por defecto (ajusta según tu lógica)
       const empresaPorDefecto = 1;
       const sedePorDefecto = 1;
-
       dbUser = await this.prisma.usuarios.create({
         data: {
           email: user.email!,
@@ -52,7 +55,6 @@ export class SupabaseAuthGuard implements CanActivate {
       });
     }
 
-    // 3. Asignar en request.user los datos de la tabla local
     request.user = {
       id_usuario: Number(dbUser.id_usuario),
       id_empresa: Number(dbUser.id_empresa),
